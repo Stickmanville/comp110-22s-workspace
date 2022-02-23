55d274b31a991d981a9f9cfcddd14297c77bc257
Stickmanville <stickmanville@gmail.com>
2022-02-23 16:16:40 -0500

    backup 3

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,4 +1,4 @@
-"""Tests utils."""
+"""Tests the functions of utils."""
 
 from exercises.ex05.utils import only_evens, sub, concat
 
@@ -22,25 +22,39 @@ def test_only_evens_no_numbers() -> None:
 
 
 def test_sub_some_numbers() -> None:
-    """Use case test."""
+    """Use case test given two lists."""
     xs: list[int] = [10, 20, 30, 40]
     assert sub(xs, 1, 3) == [20, 30]
 
 
-def test_sub_empty_list() -> None:
-    """Edge case with empty list."""
-    xs: list[int] = []
-    assert sub(xs, 0, 3) == []
-
-
 def test_sub_negative_index() -> None:
-    """Edge case with negative index."""
+    """Use case test with a negative start index."""
     xs: list[int] = [10, 20, 30, 40]
     assert sub(xs, -1, 3) == [10, 20, 30]
 
 
-def test_concat() -> None:
-    """"""
+def test_sub_empty_list() -> None:
+    """Edge case test with an empty list."""
+    xs: list[int] = []
+    assert sub(xs, 0, 3) == []
+
+
+def test_concat_two_full_lists() -> None:
+    """Use case test adding two lists."""
     list_a: list[int] = [10, 20]
     list_b: list[int] = [30, 40]
     assert concat(list_a, list_b) == [10, 20, 30, 40]
+
+
+def test_concat_one_empty_list() -> None:
+    """Use case test adding an empty list to a list."""
+    list_a: list[int] = [10, 20]
+    list_b: list[int] = []
+    assert concat(list_a, list_b) == [10, 20]
+
+
+def test_concat_two_empty_lists() -> None:
+    """Edge case test adding two empty lists together."""
+    list_a: list[int] = []
+    list_b: list[int] = []
+    assert concat(list_a, list_b) == []
