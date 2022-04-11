3410860d3eb7557fb4415e304af30aae1926b7ac
Stickmanville <stickmanville@gmail.com>
2022-04-11 19:10:45 -0400

    bk3

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -1,13 +1,18 @@
-"""Utility class for numerical operations."""
+"""Ex09. Utility class for numerical operations."""
 
 from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730332997"
 
 
 class Simpy:
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float]) -> None:
+        self.values = values
+
+    def __str__(self) -> str:
+        return f"Simpy({self.values})"
+         
